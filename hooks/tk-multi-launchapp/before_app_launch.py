e082855344f8bbb2fa970a8276f816e989982724
Anthony Kramer <anthony.kramer@gmail.com>
2020-01-20 20:23:01 -0600

    Changes to nuke-writenode settings - changed nuke-writenode 'review' output to jpg - added 'output' flag option to 'out' setting - cleaned up "before_app_launch.py" env variable settings

--- hooks/tk-multi-launchapp/before_app_launch.py
+++ hooks/tk-multi-launchapp/before_app_launch.py
@@ -51,18 +51,31 @@ class BeforeAppLaunch(sgtk.Hook):
         current_context = self.parent.context
         self.logger.debug("[CBFX] current context: %s" % current_context)
 
-        # get the details of the resolved color config from shotgun
+        # load up the tk-framework-cbfx
         cbfx_fw = self.load_framework("tk-framework-cbfx_v1.0.x")
         cbfx_utils = cbfx_fw.import_module("utils")
-        ocio_config_path_template = self.sgtk.templates["ocio_config_path"]
+
+        # make a dict to store env variables
+        env = {}
+
         if engine_name in ["tk-nuke", "tk-aftereffects"]:
-            os.environ["OCIO"] = cbfx_utils.resolve_template(ocio_config_path_template, current_context)
+            # get the details of the resolved color config from shotgun
+            ocio_config_path_template = self.sgtk.templates["ocio_config_path"]
+            ocio_path = cbfx_utils.resolve_template(ocio_config_path_template, current_context)
+            env["OCIO"] = ocio_path
+
+        # if engine_name == "tk-nuke":
+        #     env["NUKE_ALLOW_GIZMO_SAVING"] = "1"
+
+        for k, v in env.iteritems():
+            self.logger.debug("[CBFX] Setting env: {}={}".format(k, v))
+            os.environ[k] = v
 
         # Sets the current task to in progress
         if self.parent.context.task:
             task_id = self.parent.context.task['id']
             task = self.parent.sgtk.shotgun.find_one("Task", filters=[["id", "is", task_id]], fields=["sg_status_list"])
-            self.logger.debug("[CBFX] task %s status is %s" % (task_id, task['sg_status_list']))
+            self.logger.debug("[CBFX] task {} status is {}".format(task_id, task['sg_status_list']))
             if task['sg_status_list'] == 'rdy':
                 data = {
                     'sg_status_list': 'ip'
